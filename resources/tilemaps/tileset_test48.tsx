31823e1a91bbf191dfc0b929165b0c48cf1a4390
Gillian Arlabosse <62660428+G-Arlabosse@users.noreply.github.com>
2025-06-15 01:45:55 +0200

    Update textures

--- resources/tilemaps/tileset_test48.tsx
+++ resources/tilemaps/tileset_test48.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tileset_test48" tilewidth="48" tileheight="48" tilecount="4" columns="2">
- <image source="../sprites/map_tileset.png" width="96" height="96"/>
+ <image source="../textures/map_tileset.png" width="96" height="96"/>
 </tileset>
